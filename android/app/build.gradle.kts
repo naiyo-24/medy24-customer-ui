a976b84e30d964bafa9bad27a3e07041d1a49f5c
HypothetiCoder <das.subhankar7008@gmail.com>
2026-09-12 17:19:13 +0530

    Configure Android release build settings, edge-to-edge display, permissions, and bump app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,6 +54,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
     }
 }
--- android/app/src/main/kotlin/com/medy24/customer/MainActivity.kt
+++ android/app/src/main/kotlin/com/medy24/customer/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.medy24.customer
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
